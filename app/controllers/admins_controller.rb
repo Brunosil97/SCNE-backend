6b72a9e6a2205cae8a03ecb8fdf9302951035bb8
Bruno Silva <brunosilvinosilva@gmail.com>
2020-04-27 13:10:33 +0100

    attempting to edit a song with form inputs

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -3,7 +3,7 @@ class AdminsController < ApplicationController
     def sign_in
         admin = Admin.find_by(username: params[:username])
         if admin && admin.authenticate(params[:password])
-          render json: {username: admin.username, token: generate_token(id: admin.id) }
+          render json: {username: admin.username, token: generate_token({id: admin.id}) }
         else
           render json: { error: "Password is invalid "}
         end
@@ -11,7 +11,7 @@ class AdminsController < ApplicationController
     
       def validate
         if get_admin
-          render json: {username: get_admin.username, token: generate_token(id: get_admin.id)}
+          render json: {username: get_admin.username, token: generate_token({id: get_admin.id})}
         else
           render json: { error: "You are not authorized" }
         end
