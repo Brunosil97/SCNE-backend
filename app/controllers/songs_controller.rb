d251c76f71dee5b9afb2fc864c409d603da65763
Bruno Silva <brunosilvinosilva@gmail.com>
2020-04-28 12:19:08 +0100

    added function to update song and custom route

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,18 +14,16 @@ class SongsController < ApplicationController
     end
 
     def create 
-        song = Song.create(title: params[:title], artist: params[:artist], spotify: params[:spotify], soundcloud: params[:soundcloud])
+        song = Song.create(song_params)
         render json: song.to_json
        
     end 
 
-    def update 
+    def update
         song = Song.find_by(id: params[:id])
         song.update(image: params[:image])
-        byebug
         image_url = rails_blob_path(song.image)
         render json: {song: song, image_url: image_url}
-        byebug
     end 
 
     def destroy
@@ -34,4 +32,27 @@ class SongsController < ApplicationController
         songs = Song.all.with_attached_image
         render json: songs, each_serializer: SongSerializer
     end 
+
+    def update_song
+        song = Song.find_by(id: params[:id])
+        song.update(song_params)
+        song.save 
+        render json: song.to_json
+    end
+
+    def upload_image_and_attach_to_song
+
+    end
+
+    def update_song_image
+
+    end
+
+    private 
+
+    def song_params
+        params.require(:song).permit(:title, :artist, :spotify, :soundcloud)
+    end
+
+
 end
