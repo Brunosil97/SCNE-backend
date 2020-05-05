e9f0c027764c3484deed1d36a882741668dd0e84
Bruno Silva <brunosilvinosilva@gmail.com>
2020-05-05 13:38:57 +0100

    rechanged

--- db/migrate/20200422121625_create_admins.rb
+++ db/migrate/20200422121625_create_admins.rb
@@ -3,7 +3,7 @@ class CreateAdmins < ActiveRecord::Migration[6.0]
     create_table :admins do |t|
       t.string :username
       t.string :password_digest
-
+      
       t.timestamps
     end
   end
